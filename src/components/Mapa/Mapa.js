8366a9c37e42e0225259e6c6129b3613a2793726
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-03-21 17:23:22 -0300

    agrega comunas de chile

--- src/components/Mapa/Mapa.js
+++ src/components/Mapa/Mapa.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { Source, Layer } from 'react-map-gl'
+import { dataLayer } from './map-style.js';
+import data from '../../geojsons/chile/comunas.json'
 
 const Mapa = () => {
   const [viewport, setViewport] = useState({
@@ -18,12 +20,32 @@ const Mapa = () => {
     })
   }
 
+  const construirData = () => {
+    const datosFormateados = {
+      type: 'FeatureCollection',
+      geometries: data.objects.comunas.geometries
+    }
+    console.log(data)
+    return data
+  }
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {type: 'Feature', geometry: {type: 'Point', coordinates: [-122.4, 37.8]}}
+    ]
+  };
   return (
     <ReactMapGL
       {...viewport}
       onViewportChange={cambioEnElViewport}
       mapboxApiAccessToken="pk.eyJ1IjoiYWxlNjE1IiwiYSI6ImNqbDZ5eGt3ZDAxcGszdm83Z3piZ3YwdTcifQ.0dSxbx5BR0aoOsarUYmArQ"
-    />
+    >
+      {data &&
+        <Source id="test" type="geojson" data={data}>
+          <Layer {...dataLayer} />
+        </Source>
+      }
+    </ReactMapGL>
   )
 }
 
