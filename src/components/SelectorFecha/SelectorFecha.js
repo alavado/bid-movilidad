e95b1642f821a58d150555395b229cbf7000269d
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-03-24 21:22:18 -0300

    cambia de lugar los controles

--- src/components/SelectorFecha/SelectorFecha.js
+++ src/components/SelectorFecha/SelectorFecha.js
@@ -16,11 +16,11 @@ const SelectorFecha = () => {
 
   return (
     <div className="SelectorFecha">
-      <input type="range" step="1" min="0" max="180" value={dia} onChange={e => dispatch(fijarDia(parseInt(e.target.value)))} />
       <div className="SelectorFecha__acciones">
         <FontAwesomeIcon title="Día anterior" className="SelectorFecha__accion" icon={iconoAnterior} onClick={() => dispatch(fijarDia(dia - 1))} />
         <FontAwesomeIcon title="Día siguiente" className="SelectorFecha__accion" icon={iconoSiguiente} onClick={() => dispatch(fijarDia(dia + 1))} />
       </div>
+      <input type="range" step="1" min="0" max="180" value={dia} onChange={e => dispatch(fijarDia(parseInt(e.target.value)))} />
     </div>
   )
 }
