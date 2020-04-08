1f6b1f5a495fc2af496a9090e7494749c136d7e8
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-04-08 18:22:56 -0400

    responsive

--- src/components/SelectorFecha/SelectorFecha.js
+++ src/components/SelectorFecha/SelectorFecha.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fijarDia } from '../../redux/actions'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import moment from 'moment/min/moment-with-locales'
 import {
   faChevronLeft as iconoAnterior,
   faChevronRight as iconoSiguiente
@@ -16,6 +17,9 @@ const SelectorFecha = () => {
 
   return (
     <div className="SelectorFecha">
+      <div className="SelectorFecha__fecha_cercana">
+        {moment('2020-03-03').add(dia - 1, 'days').format('dddd, D [de] MMMM [de] YYYY')}
+      </div>
       <div className="SelectorFecha__acciones">
         <FontAwesomeIcon
           title="Día anterior"
