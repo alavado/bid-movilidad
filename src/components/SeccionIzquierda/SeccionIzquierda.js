ee2bd53898665aa09926ab869c35c956cbdeb752
cinai <ceinaipil@gmail.com>
2020-04-20 10:01:12 -0400

    cambiar logo ciae

--- src/components/SeccionIzquierda/SeccionIzquierda.js
+++ src/components/SeccionIzquierda/SeccionIzquierda.js
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fijarPais, fijarDestino } from '../../redux/actions'
 import { NavLink as Link } from 'react-router-dom'
 import logoIIEP from '../../assets/logo_iiep.png'
-import logoCIAE from '../../assets/logo_ciae_nuevo.png'
+import logoCIAE from '../../assets/logo_ciae_uchile.png'
 import configPaises from '../../config/paises'
 
 const SeccionIzquierda = () => {
