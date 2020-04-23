c9fdc67baf5122e4b4b56ee62e3a81bc3e9ec311
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-04-22 20:22:33 -0400

    prueba suscripcion

---
+++ src/components/FormularioSuscripcion/FormularioSuscripcion.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './FormularioSuscripcion.css'
+import { useSelector } from 'react-redux'
+import moment from 'moment'
+
+const FormularioSuscripcion = () => {
+
+  return (
+    <div className="FormularioSuscripcion">
+      <div className="FormularioSuscripcion__contenedor">
+        <h1>Prueba suscripcion</h1>
+        <form name="suscripciones" netlify>
+          <label>E-mail: <input type="email" name="email" /></label>
+          <button type="submit">Suscribir</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default FormularioSuscripcion
