ecd7f3fb6de0f4a7785d632ec972ad5c6a3f0d3a
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-04-29 22:27:47 -0400

    cambia titulo en ingles

--- src/strings/superior.js
+++ src/strings/superior.js
@@ -1,5 +1,5 @@
 const strings = {
-  titulo: { es: 'Mapa de movilidad de las personas', en: 'People mobility map' },
+  titulo: { es: 'Mapa de movilidad de las personas', en: 'Human mobility map' },
   pantallaCompleta: { es: 'Pantalla completa', en: 'Fullscreen mode' },
   salirDePantallaCompleta: { es: 'Salir de pantalla completa', en: 'Exit fullscreen mode' }
 }
